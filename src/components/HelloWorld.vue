f9ab60e7d21eb488f0e2b768d01792937d1507f9
lorenzo <lor.abbadessa@gmail.com>
2023-11-27 18:16:14 +0100

    completa esercizio

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>MY FIRST APP WITH VITE!!</h1>
+</template>
+
+<style scoped></style>
